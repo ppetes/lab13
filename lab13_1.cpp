54e14bb48e318943f3dc398d6c3a9cee5e4cedce
ratchapon prangthong <ppete2547@gmail.com>
2023-01-10 15:47:17 +0700

    lab13_1 fish

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
+ #include <ctime>
 using namespace std;
 
-int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+long long int fibonacci(int a);
+
+ int main(){
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
+} long long int fibonacci(int y){
+    if(y >= 0){
+    if(y == 0) return 0;
+    else if(y == 1) return 1;
+    else{
+     return fibonacci(y-1) + fibonacci(y-2);
+    }
+}
+else return 0;
+    
+
 }
